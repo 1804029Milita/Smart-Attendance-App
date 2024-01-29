1e7549c8519342af251e77663d1891be1ec25bef
AdibaSultanaMIM <138275673+AdibaSultanaMIM@users.noreply.github.com>
2024-01-29 23:22:26 +0600

    SheetListActivity.java

---
+++ app/src/main/java/com/example/smartattendance/SheetListActivity.java
@@ -0,0 +1,65 @@
+package com.example.smartattendance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class SheetListActivity extends AppCompatActivity {
+    private ListView sheetList;
+    private ArrayAdapter adapter;
+    private ArrayList<String> listItems = new ArrayList();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sheet_list);
+
+        loadListItems();
+
+        sheetList = findViewById(R.id.sheetList);
+        adapter = new ArrayAdapter<>(this, R.layout.sheet_list, R.id.date_list_item, listItems);
+        sheetList.setAdapter(adapter);
+
+        sheetList.setOnItemClickListener((parent,view,position,id)-> openSheetActivity(position));
+    }
+    private void openSheetActivity(int position)
+    {
+        long [] idArray = getIntent().getLongArrayExtra("idArray");
+        int [] rollArray= getIntent().getIntArrayExtra("rollArray");
+        String[] nameArray=getIntent().getStringArrayExtra("nameArray");
+
+        Intent intent=new Intent(this,SheetActivity.class);
+        intent.putExtra("idArray", idArray);
+        intent.putExtra("rollArray", rollArray);
+        intent.putExtra("nameArray", nameArray);
+        intent.putExtra("month", listItems.get(position));
+
+        startActivity(intent);
+
+    }
+
+    private void loadListItems() {
+        Cursor cursor = new DbHelper(this).getDistinctMonths();
+        Log.i("1234567890", "loadListItems: " + cursor.getCount());
+
+        while (cursor.moveToNext()) {
+            int columnIndex = cursor.getColumnIndex(DbHelper.DATE_KEY);
+            if (columnIndex >= 0) {
+                String date = cursor.getString(columnIndex);
+                listItems.add(date.substring(3));
+            } else {
+                Log.e("SheetListActivity", "Column DATE_KEY not found in cursor");
+            }
+        }
+        cursor.close();
+    }
+}
+
+
